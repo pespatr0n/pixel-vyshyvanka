982274fe38782ca756b05caadb56ef2af22c061a
Zaiats Andrii <andriy.s.zayats@gmail.com>
2026-05-18 22:16:44 +0300

    feat: add PNG export functionality with save button in UI

--- src/main/java/ua/university/Controller.java
+++ src/main/java/ua/university/Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 
 public class Controller {
     @FXML private Canvas canvas;
@@ -38,6 +39,54 @@ public class Controller {
         if (col >= 0 && col < GRID_SIZE && row >= 0 && row < GRID_SIZE) {
             grid[row][col] = colorPicker.getValue();
             redraw();
+        }
+    }
+
+    @FXML
+    public void handleSave() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Зображення PNG (*.png)", "*.png")
+        );
+        java.io.File file = fileChooser.showSaveDialog(canvas.getScene().getWindow());
+
+        if (file != null) {
+            try {
+                int scale = 20;
+                int imgSize = GRID_SIZE * scale;
+
+                java.awt.image.BufferedImage img = new java.awt.image.BufferedImage(
+                        imgSize,
+                        imgSize,
+                        java.awt.image.BufferedImage.TYPE_INT_ARGB
+                );
+
+                java.awt.Graphics2D g2d = img.createGraphics();
+
+                for (int y = 0; y < GRID_SIZE; y++) {
+                    for (int x = 0; x < GRID_SIZE; x++) {
+                        javafx.scene.paint.Color fxColor = grid[y][x];
+
+                        java.awt.Color awtColor = new java.awt.Color(
+                                (float) fxColor.getRed(),
+                                (float) fxColor.getGreen(),
+                                (float) fxColor.getBlue(),
+                                (float) fxColor.getOpacity()
+                        );
+
+                        g2d.setColor(awtColor);
+
+                        g2d.fillRect(x * scale, y * scale, scale, scale);
+                    }
+                }
+
+                g2d.dispose();
+
+                javax.imageio.ImageIO.write(img, "png", file);
+
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
